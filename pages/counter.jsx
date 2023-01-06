33323af9270385dbc3bc0d485f7eda1e2ac55323
Sumit Pokhriyal <sumitstan1998@gmail.com>
2023-01-06 17:43:05 +0530

    completed counter

--- pages/counter.jsx
+++ pages/counter.jsx
@@ -1,16 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Counter = () => {
-      const [count,setCount]=useState(0)
+  const [count, setCount] = useState(0);
   return (
     <div>
-        <h1>Count: {count}</h1>
-        <div>
-            <button onClick={()=>setCount(count+1)}>INC</button>
-            <button onClick={()=>setCount(count-1)}>DEC</button>
-        </div>
+      <h1>Count: {count}</h1>
+      <div>
+        <button onClick={() => setCount(count - 1)}>DEC</button>
+        <button onClick={() => setCount(count + 1)}>INC</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Counter
+export default Counter;
